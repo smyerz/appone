63cd95cb9a610848298ae39d44a4aeef4ec00cb1
Sarah Myers <mrsmyerz@outlook.com>
2023-10-05 15:05:09 -0700

    .
    Signed-off-by: Sarah Myers <mrsmyerz@outlook.com>

--- src/App.js
+++ src/App.js
@@ -3,16 +3,17 @@ import './App.css';
 import Card from './Card';
 
 function Logo(props) {
-  const userPic = <img src={avatar}/>;
+  const userPic = <img src={avatar} alt="cat with pizza in mouth"/>;
   return userPic;
 }
+
 //all items must be declared in App function to be seen online
 //references must be located in the src folder or same folder
 function App() {
   return (
     <div className='App'>
       <h1> Hello World!</h1>
-      <Logo/> 
+      <Logo /> 
       <Card h2 = "first card's h2" h3 = "first card's h3" />
       <Card h2 = "second card's h2" h3 = "second card's h3" />
       <Card h2 = "third card's h2" h3 = "third card's h3" />
