cb2724a8f45e0f4aa5b545836577b309eb809109
Sarah Myers <mrsmyerz@outlook.com>
2023-10-05 12:59:59 -0700

    .
    Signed-off-by: Sarah Myers <mrsmyerz@outlook.com>

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import avatar from './NOMNOM.png';
 import './App.css';
+import Card from './Card';
 
+function Logo(props) {
+  const userPic = <img src={avatar}/>;
+  return userPic;
+}
+//all items must be declared in App function to be seen online
+//references must be located in the src folder or same folder
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1> Hello World!</h1>
+      <Logo/> 
+      <Card h2 = "first card's h2" h3 = "first card's h3" />
+      <Card h2 = "second card's h2" h3 = "second card's h3" />
+      <Card h2 = "third card's h2" h3 = "third card's h3" />
     </div>
   );
 }
